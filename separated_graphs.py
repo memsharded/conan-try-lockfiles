928c1784480837df46152785cb0cc134844af360
jgsogo <jgsogo@gmail.com>
2020-07-16 09:31:35 +0200

    add alternatives

--- separated_graphs.py
+++ separated_graphs.py
@@ -24,6 +24,22 @@ if __name__ == '__main__':
     t.run(f"lock create --reference flac/1.0@ --lockfile-out=flac_1.0.lock")
     t.run(f"lock create --reference vorbis/1.0@ --lockfile-out=vorbis_1.0.lock")
 
-    # NO: Use two lockfiles to build a consumer one
+    # Use two lockfiles to build a consumer one
     t.save({'conanfile.txt': "[requires]\nflac/1.0\nvorbis/1.0\n"})
-    t.run(f"lock create conanfile.txt --lockfile=flac_1.0.lock --lockfile=vorbis_1.0.lock --lockfile-out=project.lock")
+    t.run(f"lock create conanfile.txt --lockfile=flac_1.0.lock --lockfile=vorbis_1.0.lock --lockfile-out=project.lock", assert_error=True)
+    # TODO: Merge input lockfiles first and then run as usual.
+    #   - approach#1: New cache, install both lockfiles and create the new one:
+    #       ```
+    #       conan install flac_1.0.lock 
+    #       conan install vorbis_1.0.lock 
+    #       conan lock create conanfile.txt --lockfile-out=project.lock
+    #       ```
+    #       Approach invalid, it doesn't raise for inconsistencies between lockfiles
+    #
+    #   - approach#2: Merge the two lockfiles as JSON
+    #       Do not store the profile in the lockfile, but store settings and options per node. Merging
+    #       two lockfiles would be as easy as adding two JSONs (raise if two nodes with the same key
+    #       contain different information).
+    #
+    #       Prerequisite.- Key for nodes is '<context>::<ref-name>' instead of a random number
+
